f86c1c2aa5306356a60f97b5693d1a03741f5cad
kadraman <kevin.lee@microfocus.com>
2023-11-03 11:22:46 +0000

    Updating GitHub Actions

--- 02-express-api/fortify-scan.ps1
+++ 02-express-api/fortify-scan.ps1
@@ -16,10 +16,10 @@ Write-Host Running translation...
 
 Write-Host Running scan...
 & sourceanalyzer '-Dcom.fortify.sca.ProjectRoot=.fortify' $ScanSwitches -b "$AppName" -verbose `
-   -build-project "$AppName" -build-version "$AppVersion" -build-label "SNAPSHOT" -scan
-#   -f "$($AppName).fpr"
+   -build-project "$AppName" -build-version "$AppVersion" -build-label "SNAPSHOT" -scan `
+   -f "$($AppName).fpr"
 
-#Write-Host Generating PDF report...
-#& ReportGenerator '-Dcom.fortify.sca.ProjectRoot=.fortify' -user "Demo User" -format pdf -f "$($AppName).pdf" -source "$($AppName).fpr"
+Write-Host Generating PDF report...
+& ReportGenerator '-Dcom.fortify.sca.ProjectRoot=.fortify' -user "Demo User" -format pdf -f "$($AppName).pdf" -source "$($AppName).fpr"
 
 Write-Host Done.
--- 03-react-native-client/fortify-scan.ps1
+++ 03-react-native-client/fortify-scan.ps1
@@ -12,14 +12,20 @@ $ScanSwitches = ""
 
 Write-Host Running translation...
 & sourceanalyzer '-Dcom.fortify.sca.ProjectRoot=.fortify' $ScanSwitches -b "$AppName" -verbose `
--exclude ".\config" ".\src" -exclude ".\node_modules" -exclude ".\src\configs\swagger_output.json"
+-jdk 11 -java-build-dir "android/app/build/intermediates/javac/debug/classes/com/iwamobile/**.*" `
+-cp "android/app/build/intermediates/javac/debug/classes:lib/*.jar" `
+-exclude ".\config" -exclude ".\__tests__" -exclude ".\.bundle" -exclude ".\node_modules" `
+"android/app/src/main/java/com/iwamobile/**/*" `
+"android/app/src/debug/java/com/iwamobile/**/*" `
+"app/**/*"
 
 Write-Host Running scan...
 & sourceanalyzer '-Dcom.fortify.sca.ProjectRoot=.fortify' $ScanSwitches -b "$AppName" -verbose `
-   -build-project "$AppName" -build-version "$AppVersion" -build-label "SNAPSHOT" -scan
-#   -f "$($AppName).fpr"
+   -java-build-dir "android/app/build/intermediates/javac/debug/classes/com/iwamobile/**.*" `
+   -build-project "$AppName" -build-version "$AppVersion" -build-label "SNAPSHOT" -scan `
+   -f "$($AppName).fpr"
 
-#Write-Host Generating PDF report...
-#& ReportGenerator '-Dcom.fortify.sca.ProjectRoot=.fortify' -user "Demo User" -format pdf -f "$($AppName).pdf" -source "$($AppName).fpr"
+Write-Host Generating PDF report...
+& ReportGenerator '-Dcom.fortify.sca.ProjectRoot=.fortify' -user "Demo User" -format pdf -f "$($AppName).pdf" -source "$($AppName).fpr"
 
 Write-Host Done.
